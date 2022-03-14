1d7e43db66fe476c29667fd3fb060bc1bb68027e
Liav <liav.acc@gmail.com>
2022-03-14 17:31:58 +0200

    completed checkout page

--- index.js
+++ index.js
@@ -23,18 +23,28 @@ rexailApp.controller('appController', function ($http, $scope, $filter, $locatio
     ctrl.state = {
         cartItems: [],
         total: '0.00',
-        userComment: '',
         selectedCategory: null,
         selectedSortBy: null,
+        formControl: {
+            userComment: '',
+            cardHolderID: null,
+        },
         data: {
             storeData: [],
             categoriesData: []
-        }, errors: {
+        },
+        errors: {
             cart: {
                 userComment: false,
                 productComment: false
             },
-            checkout: {}
+            checkout: {
+                cardHolderName: null,
+                cardHolderID: null,
+                cardNumber: null,
+                cardExpirationDate: null,
+                cardCVV: null,
+            }
         }
     }
 
@@ -76,20 +86,22 @@ rexailApp.controller('appController', function ($http, $scope, $filter, $locatio
 
     ctrl.removeProduct = function (product) {
         product.quantity = null
-        ctrl.state.cartItems = ctrl.state.cartItems.filter(item => item !== product)
+        ctrl.state.cartItems = ctrl.state.cartItems.filter(item => item.id !== product.id)
         ctrl.state.total = calculateTotal();
     }
 
     ctrl.increaseProductQuantity = function (product) {
-        if (!product.primaryQuantityUnit) product.primaryQuantityUnit = {'amountJumps': 1}
+        if (!product.primaryQuantityUnit) { product.primaryQuantityUnit = {'amountJumps': 1} }
         product.quantity = product.quantity ? product.quantity + product.primaryQuantityUnit.amountJumps : product.primaryQuantityUnit.amountJumps;
         !ctrl.state.cartItems.includes(product) && ctrl.state.cartItems.unshift(product)
         ctrl.state.total = calculateTotal();
     }
 
     ctrl.decreaseProductQuantity = function (product) {
-        if (product.quantity > product.primaryQuantityUnit.amountJumps) product.quantity = product.quantity - product.primaryQuantityUnit.amountJumps
-        !ctrl.state.cartItems.includes(product) && ctrl.state.cartItems.unshift(product)
+        if (product.quantity > product.primaryQuantityUnit.amountJumps) {
+            product.quantity = product.quantity - product.primaryQuantityUnit.amountJumps
+            !ctrl.state.cartItems.includes(product) && ctrl.state.cartItems.unshift(product)
+        } else ctrl.removeProduct(product)
         ctrl.state.total = calculateTotal();
     }
 
@@ -108,7 +120,33 @@ rexailApp.controller('appController', function ($http, $scope, $filter, $locatio
         if (value === 'מוצרים במבצע') ctrl.state.selectedCategory.children = $filter('orderBy')(ctrl.state.selectedCategory.children, 'promoted', true);
     }
 
-    //Cart validations
+    // Checkout validations
+    ctrl.validateFormInput = function (value) {
+        if (value === 'card-holder-name') ctrl.state.errors.checkout.cardHolderName = validateCardHolderName(ctrl.state.formControl.cardHolderName) ? false : true
+        if (value === 'card-holder-id') ctrl.state.errors.checkout.cardHolderID = validateCardHolderID(ctrl.state.formControl.cardHolderID) ? false : true
+        if (value === 'card-number') ctrl.state.errors.checkout.cardNumber = validateCardNumber(ctrl.state.formControl.cardNumber) ? false : true
+        if (value === 'card-expiration-date') ctrl.state.errors.checkout.cardExpirationDate = validateCardExpirationDate(ctrl.state.formControl.cardExpirationDate) ? false : true
+        if (value === 'card-cvv') ctrl.state.errors.checkout.cardCVV = validateCardCVV(ctrl.state.formControl.cardCVV) ? false : true
+    }
+
+    ctrl.addSlashExp = function () {
+        let expire_date = document.getElementById('exp').value;
+        if (expire_date.length == 2){
+            document.getElementById('exp').value = expire_date +'/';
+        }
+    }
+
+    // Enable or Disable submit checkout button
+    ctrl.disableCheckoutForm = function () {
+        let formValidations = Object.values(ctrl.state.errors.checkout)
+
+        // checks whether an input form is invalid
+        const isNotValid = (element) => (element === true || element === null);
+
+        return formValidations.some(isNotValid)
+    }
+
+    // Cart validations
     ctrl.validateCart = function () {
         // Getting all products with preparation order prop
         let prepProducts = ctrl.state.cartItems.filter(product => product.commentType);
@@ -116,7 +154,7 @@ rexailApp.controller('appController', function ($http, $scope, $filter, $locatio
         let validPrepProducts = prepProducts.filter(product => product.commentType && product.comment)
 
         // Check if user comment is empty
-        ctrl.state.errors.cart.userComment = !ctrl.state.userComment.length
+        ctrl.state.errors.cart.userComment = !ctrl.state.formControl.userComment.length
 
         // Check if all product's preparation methods are selected
         ctrl.state.errors.cart.productComment = (validPrepProducts.length !== prepProducts.length)
@@ -125,8 +163,13 @@ rexailApp.controller('appController', function ($http, $scope, $filter, $locatio
         if (!ctrl.state.errors.cart.userComment && !ctrl.state.errors.cart.productComment) $location.path('/checkout').replace();
     };
 
-    // Setting imgBaseUrl
+    // ctrl constants
     ctrl.imgBaseUrl = 'https://s3.eu-central-1.amazonaws.com/images-il.rexail.com/'
+
+
+    /* ~~ $scope functions: ~~ */
+    // Convert str to num expression in order to use in template
+    $scope.parseFloat = parseFloat;
 
     // Setting initial value for pagination limit (infinite scroll)
     $scope.paginationLimit = 20
@@ -213,7 +256,6 @@ rexailApp.directive('navMenu', function () {
     }
 })
 
-// Util Functions
 function formatData(array) {
     return Object.values(array.reduce((obj, current) => {
         if (!obj[current.productCategory.id]) {
@@ -249,3 +291,43 @@ function formatData(array) {
         0: {id: 0, name: 'כל המוצרים', children: []}, 1: {id: 1, name: 'מבצעים', children: []}
     }))
 }
+
+// Util Functions
+function validateCardHolderID(IDnum) {
+    if (!IDnum) return false
+    if (IDnum.length < 9) { while(IDnum.length < 9) { IDnum = '0' + IDnum; } }
+
+    let id = String(IDnum).trim();
+    if ((id.length > 9) || (id.length < 5)) return false;
+    if (isNaN(id)) return false;
+
+    return Array.from(id, Number).reduce((counter, digit, i) => {
+        const step = digit * ((i % 2) + 1);
+        return counter + (step > 9 ? step - 9 : step);
+    }) % 10 === 0;
+}
+
+function validateCardHolderName(string) {
+    const validate = /^[a-zA-Z\u0590-\u05FF\u200f\u200e]([-']?[a-zA-Z\u0590-\u05FF\u200f\u200e]+)*( [a-zA-Z\u0590-\u05FF\u200f\u200e]([-']?[a-zA-Z\u0590-\u05FF\u200f\u200e]+)*)+$/;
+    return validate.test(string);
+}
+
+function validateCardNumber(value) {
+    if (!value) return false
+    let string = value.trim()
+    const validate = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}(?:2131|1800|35\d{3})\d{11})$/;
+    return validate.test(string);
+}
+
+function validateCardExpirationDate(value) {
+    let string = String(value)
+    const validate = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/;
+    return validate.test(string);
+}
+
+function validateCardCVV(value) {
+    let string = String(value)
+    const validate = /^[0-9]{3,4}$/;
+    return validate.test(string)
+}
+
