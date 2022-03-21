9b4beecf35b430c4148c6afd0c4f772e480cbcc9
Liav <liav.acc@gmail.com>
2022-03-21 15:40:27 +0200

    angular module appController --> angular module run

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const rexailApp = angular.module('rexail-app', ['ngRoute', 'infinite-scroll', 'storeModule', 'cartModule', 'checkoutModule']);
 
+// Constants
 rexailApp.constant('IMG_BASE_URL', 'https://s3.eu-central-1.amazonaws.com/images-il.rexail.com/');
 rexailApp.constant('CURRENCY_SIGN', '₪');
 
+// Routes
 rexailApp.config(function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode({
         enabled: true,
@@ -31,7 +33,8 @@ rexailApp.config(function ($routeProvider, $locationProvider) {
         });
 })
 
-rexailApp.controller('appController', function ($rootScope, $http, $filter, $location, $anchorScroll, IMG_BASE_URL) {
+// Initialization
+rexailApp.run(function ($rootScope, $http, $location, IMG_BASE_URL) {
     $rootScope.globalState = {
         storeData: {},
         categoriesData: [],
@@ -104,7 +107,7 @@ rexailApp.controller('appController', function ($rootScope, $http, $filter, $loc
     }
 })
 
-
+// Services
 rexailApp.service('cartActionsService', function ($rootScope) {
     const ctrl = this;
 
@@ -141,10 +144,9 @@ rexailApp.service('cartActionsService', function ($rootScope) {
         const sumWithInitial = $rootScope.globalState.cartItems.reduce((totalSum, product) => totalSum + product['price'] * product.quantity, initialValue)
         return sumWithInitial.toFixed(2)
     }
-
 })
 
-
+// Controllers
 rexailApp.controller('productController', function (CURRENCY_SIGN) {
     const ctrl = this;
 
@@ -152,7 +154,7 @@ rexailApp.controller('productController', function (CURRENCY_SIGN) {
 
     ctrl.onUnitTypeChange = function (product, quantityUnit) {
         // Check the new unit weight and modify the price accordingly, if the new unit weight is higher, than multiply the price by it,
-        // else, divide the price by the previous unit weight value.
+        // else, divide the price by the previous unit weight value, do the same for old price.
         if (product.primaryQuantityUnit.estimatedUnitWeight < quantityUnit.estimatedUnitWeight) {
             product.price = (product.price * quantityUnit.estimatedUnitWeight).toFixed(2)
             if (product.originalPrice) {
@@ -180,150 +182,62 @@ rexailApp.controller('productController', function (CURRENCY_SIGN) {
     }
 })
 
-// components
-    rexailApp.directive('storeItem', function () {
-        return {
-            templateUrl: 'directives/store-item.html',
-            replace: true,
-            scope: {
-                product: '=',
-                onIncreaseProductQuantity: '&',
-                onDecreaseProductQuantity: '&',
-            },
-            controller: 'productController',
-            controllerAs: 'ctrl',
-        }
-    })
-
-    rexailApp.directive('itemPreview', function () {
-        return {
-            templateUrl: 'directives/item-preview.html',
-            replace: true,
-            scope: {
-                product: '=',
-                onRemoveProduct: '&',
-                onIncreaseProductQuantity: '&',
-                onDecreaseProductQuantity: '&'
-            },
-            controller: 'productController',
-            controllerAs: 'ctrl',
-        }
-    })
-
-    rexailApp.directive('cartItem', function () {
-        return {
-            templateUrl: 'directives/cart-item.html',
-            replace: true,
-            scope: {
-                product: '=',
-                onIncreaseProductQuantity: '&',
-                onDecreaseProductQuantity: '&',
-                onRemoveProduct: '&',
-                errors: '=',
-            },
-            controller: 'productController',
-            controllerAs: 'ctrl',
-        }
-    })
-
-    rexailApp.directive('footerMenu', function () {
-        return {
-            templateUrl: 'directives/footer-menu.html', replace: true,
-        }
-    })
-
-    rexailApp.directive('navMenu', function () {
-        return {
-            templateUrl: 'directives/header-menu.html',
-            replace: true,
-
-
-        }
-    })
-
-//
-// function formatData(array) {
-//     return Object.values(array.reduce((obj, current) => {
-//         if (!obj[current.productCategory.id]) {
-//             obj[current.productCategory.id] = {
-//                 id: current.productCategory.id,
-//                 name: current.productCategory.name,
-//                 children: []
-//             }
-//         }
-//
-//         // If there is no primary quantity unit than use the first product selling unit.
-//         let primaryQuantityUnit = !current.product.primaryQuantityUnit && current.productSellingUnits[0]
-//
-//         let productModel = {
-//             id: current.id,
-//             name: current.product.name,
-//             fullName: current.fullName,
-//             imageUrl: current.imageUrl,
-//             price: current.price,
-//             promoted: current.promoted,
-//             originalPrice: current.originalPrice,
-//             productQuality: current.productQuality,
-//             currentRelevancy: current.currentRelevancy,
-//             primaryQuantityUnit: current.primaryQuantityUnit ? current.primaryQuantityUnit : current.productSellingUnits[0],
-//             productSellingUnits: current.productSellingUnits,
-//             commentType: current.commentType
-//         }
-//
-//         // Push to all products category
-//         if (current.product.id) obj['0'].children.push(productModel)
-//         // Push to promoted products category
-//         if (current.promoted) obj['1'].children.push(productModel)
-//
-//         // Continue reformatting
-//         obj[current.productCategory.id].children.push(productModel)
-//
-//         return obj
-//     }, {
-//         // Initial state manually with default categories
-//         0: {id: 0, name: 'כל המוצרים', children: []}, 1: {id: 1, name: 'מבצעים', children: []}
-//     }))
-// }
-
-// Regex Util Functions (copied from regex101 library)
-    function validateCardHolderID(IDnum) {
-        if (!IDnum) return false
-        if (IDnum.length < 9) {
-            while (IDnum.length < 9) {
-                IDnum = '0' + IDnum;
-            }
-        }
-
-        let id = String(IDnum).trim();
-        if ((id.length > 9) || (id.length < 5)) return false;
-        if (isNaN(id)) return false;
-
-        return Array.from(id, Number).reduce((counter, digit, i) => {
-            const step = digit * ((i % 2) + 1);
-            return counter + (step > 9 ? step - 9 : step);
-        }) % 10 === 0;
+// Directives
+rexailApp.directive('storeItem', function () {
+    return {
+        templateUrl: 'directives/store-item.html',
+        replace: true,
+        scope: {
+            product: '=',
+            onIncreaseProductQuantity: '&',
+            onDecreaseProductQuantity: '&',
+        },
+        controller: 'productController',
+        controllerAs: 'ctrl',
     }
+})
 
-    function validateCardHolderName(string) {
-        const validate = /^[a-zA-Z\u0590-\u05FF\u200f\u200e]([-']?[a-zA-Z\u0590-\u05FF\u200f\u200e]+)*( [a-zA-Z\u0590-\u05FF\u200f\u200e]([-']?[a-zA-Z\u0590-\u05FF\u200f\u200e]+)*)+$/;
-        return validate.test(string);
+rexailApp.directive('itemPreview', function () {
+    return {
+        templateUrl: 'directives/item-preview.html',
+        replace: true,
+        scope: {
+            product: '=',
+            onRemoveProduct: '&',
+            onIncreaseProductQuantity: '&',
+            onDecreaseProductQuantity: '&'
+        },
+        controller: 'productController',
+        controllerAs: 'ctrl',
     }
+})
 
-    function validateCardNumber(value) {
-        if (!value) return false
-        let string = value.trim()
-        const validate = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}(?:2131|1800|35\d{3})\d{11})$/;
-        return validate.test(string);
+rexailApp.directive('cartItem', function () {
+    return {
+        templateUrl: 'directives/cart-item.html',
+        replace: true,
+        scope: {
+            product: '=',
+            onIncreaseProductQuantity: '&',
+            onDecreaseProductQuantity: '&',
+            onRemoveProduct: '&',
+            errors: '=',
+        },
+        controller: 'productController',
+        controllerAs: 'ctrl',
     }
+})
 
-    function validateCardExpirationDate(value) {
-        let string = String(value)
-        const validate = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/;
-        return validate.test(string);
+rexailApp.directive('footerMenu', function () {
+    return {
+        templateUrl: 'directives/footer-menu.html',
+        replace: true,
     }
+})
 
-    function validateCardCVV(value) {
-        let string = String(value)
-        const validate = /^[0-9]{3,4}$/;
-        return validate.test(string)
+rexailApp.directive('navMenu', function () {
+    return {
+        templateUrl: 'directives/header-menu.html',
+        replace: true,
     }
+})
