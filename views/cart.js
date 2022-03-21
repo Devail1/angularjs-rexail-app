122e8d7feeebe7e5d4fc35d0fdc50fd5694f7bce
Liav Edry <liav.acc@gmail.com>
2022-03-21 22:42:36 +0200

    revert bonus changes

--- views/cart.js
+++ views/cart.js
@@ -8,5 +8,24 @@ cart.controller("cartController", function (cartActionsService, CURRENCY_SIGN, $
         formControl: {
             userComment: '',
         },
+        errors: {
+            productComment: false
+        },
     }
+
+    // Cart products required comment validations
+    ctrl.onCartSubmit = function () {
+        // Getting all products with comments
+        let prepProducts = $rootScope.globalState.cartItems.filter(product => product.commentType);
+        // Getting all products with selected comment
+        let validPrepProducts = prepProducts.filter(product => product.commentType && product.comment)
+        // Check if all product's preparation methods are selected
+        ctrl.state.errors.productComment = (validPrepProducts.length !== prepProducts.length)
+
+        // Check if user comment is empty
+        ctrl.state.errors.userComment = !ctrl.state.formControl.userComment.length
+
+        // Redirect to /checkout if there are no errors
+        if (!ctrl.state.errors.userComment && !ctrl.state.errors.productComment) $location.path('/checkout').replace();
+    };
 });
